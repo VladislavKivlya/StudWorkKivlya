04739d2904ba4d03fbe937a84298502613a3d16d
VladislavKivlya <62392297+VladislavKivlya@users.noreply.github.com>
2020-03-21 00:37:22 +0700

    lesson18

---
+++ regular_expressions/lesson18/script.js
@@ -0,0 +1,9 @@
+//2
+let site = 'https://github.com';
+alert(/https?:\/\//.test(site));
+//4
+let img = 'a.jpeg';
+alert(/\.jpe?g/.test(img));
+// 6
+let num = "1634567";
+alert(/\d{12}/.test(num));
